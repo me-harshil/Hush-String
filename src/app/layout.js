3dcf0f268b938b00ae3c1e1e58437a223dd270bd
Harshil <harshil0408patel@gmail.com>
2023-10-18 09:39:09 +0530

    Add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react"; // Analytics
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'HushString',
-  description: 'HushString - Where Melodies Whisper on Strings',
-}
+  title: "HushString",
+  description: "HushString - Where Melodies Whisper on Strings",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
